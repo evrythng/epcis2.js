9746814a8b6787313e71bcfd1d01d39d85599465
clementh59 <hecquet.c59@gmail.com>
2021-05-04 14:56:01 +0200

    applied linter

--- example/index.js
+++ example/index.js
@@ -1,10 +1,10 @@
-const { ObjectEvent, actionTypes, bizSteps, setup } = require('epcis2');
+const { ObjectEvent, actionTypes, bizSteps, setup } = require('epcis2')
 
 setup({
-  endpoint: 'https://epcis.evrythng.io/v2_0/',
-});
+  endpoint: 'https://epcis.evrythng.io/v2_0/'
+})
 
-const objectEvent = new ObjectEvent();
+const objectEvent = new ObjectEvent()
 
 objectEvent
   .setEventID('...ID')
@@ -12,12 +12,12 @@ objectEvent
   .setBizStep(bizSteps.inspecting)
   .addEPC('urn:epc:id:sgtin:4012345.011111.9876')
   .addEPC('urn:epc:id:sgtin:4012345.011111.9877') // You can add multiple EPCs to an event
-  .setReadPoint('urn:epc:id:sgln:4012345.00005.0');
+  .setReadPoint('urn:epc:id:sgln:4012345.00005.0')
 
-console.log(objectEvent.bizStep); // urn:epcglobal:cbv:bizstep:inspecting
-console.log(objectEvent.action); // OBSERVE
+console.log(objectEvent.bizStep) // urn:epcglobal:cbv:bizstep:inspecting
+console.log(objectEvent.action) // OBSERVE
 
-console.log(objectEvent.toJSON());
+console.log(objectEvent.toJSON())
 /* output:
 {
   isA: 'ObjectEvent',
