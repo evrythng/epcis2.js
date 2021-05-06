d126665ed2dff86c679496aa89586191b0c5714e
clementh59 <hecquet.c59@gmail.com>
2021-05-06 12:31:29 +0200

    Added stronger tests for EPCISMasterData

--- test/epcis/EPCISModels.spec.js
+++ test/epcis/EPCISModels.spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import EPCISMasterData from '../../src/entity/epcis/EPCISMasterData';
+import Vocabulary from '../../src/entity/model/Vocabulary';
 
 const exampleEPCISHeader = {
   epcisMasterData: {
@@ -96,5 +97,40 @@ describe('unit tests for the EPCIS classes in the src/entity/epcis folder', () =
       const e = new EPCISMasterData(exampleEPCISHeader.epcisMasterData);
       expect(e.toObject()).to.deep.equal(exampleEPCISHeader.epcisMasterData);
     });
+
+    const vocabularyList = exampleEPCISHeader.epcisMasterData
+      .vocabularyList.map((v) => new Vocabulary(v));
+
+    it('should add and remove vocabulary', async () => {
+      const e = new EPCISMasterData();
+      e.addVocabulary(vocabularyList[0]);
+      expect(e.getVocabularyList()[0]).to.deep.equal(vocabularyList[0]);
+      e.addVocabulary(vocabularyList[1]);
+      expect(e.getVocabularyList()).to.deep.equal([vocabularyList[0], vocabularyList[1]]);
+      e.removeVocabulary(vocabularyList[0]);
+      expect(e.getVocabularyList()).to.deep.equal([vocabularyList[1]]);
+      e.removeVocabulary(vocabularyList[1]);
+      expect(e.getVocabularyList()).to.deep.equal([]);
+    });
+    it('should add and remove a vocabulary List', async () => {
+      const e = new EPCISMasterData();
+      e.addVocabularyList(vocabularyList);
+      expect(e.getVocabularyList()).to.deep.equal(vocabularyList);
+      e.removeVocabularyList(vocabularyList);
+      expect(e.getVocabularyList()).to.deep.equal([]);
+    });
+    it('should clear the vocabulary List', async () => {
+      const e = new EPCISMasterData();
+      e.addVocabularyList(vocabularyList);
+      expect(e.getVocabularyList()).to.deep.equal(vocabularyList);
+      e.clearVocabularyList();
+      expect(e.getVocabularyList()).to.be.equal(undefined);
+    });
+    it('should not add the vocabulary list to JSON if it is not defined',
+      async () => {
+        const e = new EPCISMasterData();
+        const json = e.toObject();
+        expect(json.vocabularyList).to.be.equal(undefined);
+      });
   });
 });
