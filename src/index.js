98fc2e1c0174433ea7df2f43e26c05d89be24c46
clementh59 <hecquet.c59@gmail.com>
2021-05-07 17:25:01 +0200

    Added all the exports to the index file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+export { default as EPCISDocument } from './entity/epcis/EPCISDocument';
+export { default as EPCISMasterData } from './entity/epcis/EPCISMasterData';
+export { default as EPCISHeader } from './entity/epcis/EPCISHeader';
 export { default as ObjectEvent } from './entity/events/ObjectEvent';
+export { default as TransformationEvent } from './entity/events/TransformationEvent';
+export { default as AggregationEvent } from './entity/events/AggregationEvent';
+export { default as AssociationEvent } from './entity/events/AssociationEvent';
+export { default as TransactionEvent } from './entity/events/TransactionEvent';
 export { default as validateSchema } from './schema/validator';
 export { default as ErrorDeclaration } from './entity/model/ErrorDeclaration';
+export { default as AttributeElement } from './entity/model/AttributeElement';
 export { default as BizLocation } from './entity/model/BizLocation';
 export { default as BizTransactionElement } from './entity/model/BizTransactionElement';
 export { default as DestinationElement } from './entity/model/DestinationElement';
 export { default as PersistentDisposition } from './entity/model/PersistentDisposition';
 export { default as QuantityElement } from './entity/model/QuantityElement';
+export { default as Ilmd } from './entity/model/Ilmd';
 export { default as ReadPoint } from './entity/model/ReadPoint';
 export { default as SourceElement } from './entity/model/SourceElement';
 export { default as SensorElement } from './entity/model/sensor/SensorElement';
-export { default as SensorReportElement } from './entity/model/sensor/SensorReportElement';
+export { default as Vocabulary } from './entity/model/Vocabulary';
+export { default as VocabularyElement } from './entity/model/VocabularyElement';
 export { default as SensorMetadata } from './entity/model/sensor/SensorMetadata';
+export { default as SensorReportElement } from './entity/model/sensor/SensorReportElement';
 export { default as setup } from './setup';
 
 export * from './cbv/cbv';
