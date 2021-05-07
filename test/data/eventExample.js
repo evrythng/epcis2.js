a049bf2c1aef7377f444e100c70f1e54dc03e971
clementh59 <hecquet.c59@gmail.com>
2021-05-07 11:16:50 +0200

    Added more example to eventExample.js

--- test/data/eventExample.js
+++ test/data/eventExample.js
@@ -62,15 +62,26 @@ export const exampleSensorReportElement2 = {
 };
 export const exampleSensorElement = {
   sensorMetadata: {
-    time: '2019-07-19T14:00:00.000+01:00', deviceID: 'urn:epc:id:giai:4000001.111', deviceMetadata: 'https://id.gs1.org/giai/4000001111', rawData: 'https://example.org/giai/401234599999', dataProcessingMethod: 'https://example.com/gdti/4012345000054987', bizRules: 'https://example.org/gdti/4012345000054987',
+    time: '2019-07-19T14:00:00.000+01:00',
+    deviceID: 'urn:epc:id:giai:4000001.111',
+    deviceMetadata: 'https://id.gs1.org/giai/4000001111',
+    rawData: 'https://example.org/giai/401234599999',
+    dataProcessingMethod: 'https://example.com/gdti/4012345000054987',
+    bizRules: 'https://example.org/gdti/4012345000054987',
   },
   sensorReport: [
     { type: 'gs1:MT-Humidity', value: 12.1, uom: 'A93' },
     {
-      type: 'gs1:MT-Molar_concentration', chemicalSubstance: 'https://identifiers.org/inchikey:CZMRCDWAGMRECN-UGDNZRGBSA-N', value: 0.18, uom: 'C35',
+      type: 'gs1:MT-Molar_concentration',
+      chemicalSubstance: 'https://identifiers.org/inchikey:CZMRCDWAGMRECN-UGDNZRGBSA-N',
+      value: 0.18,
+      uom: 'C35',
     },
     {
-      type: 'gs1:MT-Molar_concentration', microorganism: 'https://www.ncbi.nlm.nih.gov/taxonomy/1126011', value: 0.05, uom: 'C35',
+      type: 'gs1:MT-Molar_concentration',
+      microorganism: 'https://www.ncbi.nlm.nih.gov/taxonomy/1126011',
+      value: 0.05,
+      uom: 'C35',
     },
   ],
 };
@@ -158,10 +169,10 @@ export const exampleObjectEvent = {
     type: 'urn:epcglobal:cbv:btt:po',
     bizTransaction: 'http://transaction.acme.com/po/12345678',
   },
-  {
-    type: 'urn:epcglobal:cbv:btt:po',
-    bizTransaction: 'http://transaction.acme.com/po/12345679',
-  }],
+    {
+      type: 'urn:epcglobal:cbv:btt:po',
+      bizTransaction: 'http://transaction.acme.com/po/12345679',
+    }],
   destinationList: [
     { type: 'urn:epcglobal:cbv:sdt:owning_party', destination: 'urn:epc:id:pgln:9520999.99999' },
   ],
@@ -206,10 +217,10 @@ export const exampleTransactionEvent = {
     type: 'urn:epcglobal:cbv:btt:po',
     bizTransaction: 'http://transaction.acme.com/po/12345678',
   },
-  {
-    type: 'urn:epcglobal:cbv:btt:po',
-    bizTransaction: 'http://transaction.acme.com/po/12345679',
-  }],
+    {
+      type: 'urn:epcglobal:cbv:btt:po',
+      bizTransaction: 'http://transaction.acme.com/po/12345679',
+    }],
   destinationList: [
     { type: 'urn:epcglobal:cbv:sdt:owning_party', destination: 'urn:epc:id:pgln:9520999.99999' },
   ],
@@ -237,6 +248,128 @@ export const exampleTransactionEvent = {
     unset: ['urn:epcglobal:cbv:disp:completeness_verified'],
   },
   parentID: 'parentID',
+};
+export const exampleAggregationEvent = {
+  '@context': ['https://gs1.github.io/EPCIS/epcis-context.jsonld', { 'example': 'http://ns.example.com/epcis/' }],
+
+  'id': '_:document1',
+  'isA': 'EPCISDocument',
+  'schemaVersion': '2.0',
+  'creationDate': '2005-07-11T11:30:47.0Z',
+  'epcisBody': {
+    'eventList': [
+      {
+        'eventID': 'ni:///sha-256;87b5f18a69993f0052046d4687dfacdf48f7c988cfabda2819688c86b4066a49?ver=CBV2.0',
+        'isA': 'AggregationEvent',
+        'eventTime': '2013-06-08T14:58:56.591Z',
+        'eventTimeZoneOffset': '+02:00',
+        'parentID': 'urn:epc:id:sscc:0614141.1234567890',
+        'childEPCs': ['urn:epc:id:sgtin:0614141.107346.2017', 'urn:epc:id:sgtin:0614141.107346.2018'],
+        'action': 'OBSERVE',
+        'bizStep': 'urn:epcglobal:cbv:bizstep:receiving',
+        'disposition': 'urn:epcglobal:cbv:disp:in_progress',
+        'readPoint': { 'id': 'urn:epc:id:sgln:0614141.00777.0' },
+        'bizLocation': { 'id': 'urn:epc:id:sgln:0614141.00888.0' },
+
+        'childQuantityList': [
+          { 'epcClass': 'urn:epc:idpat:sgtin:4012345.098765.*', 'quantity': 10 },
+          { 'epcClass': 'urn:epc:class:lgtin:4012345.012345.998877', 'quantity': 200.5, 'uom': 'KGM' },
+        ],
+        'example:myField': 'Example of a vendor/user extension',
+      },
+
+    ],
+  },
+
+};
+export const exampleTransformationEvent = {
+  '@context': ['https://gs1.github.io/EPCIS/epcis-context.jsonld', { 'example': 'http://ns.example.com/epcis/' }],
+
+  'id': '_:document1',
+  'isA': 'EPCISDocument',
+  'schemaVersion': '2.0',
+  'creationDate': '2013-06-04T14:59:02.099+02:00',
+  'epcisBody': {
+    'eventList': [
+      {
+        'eventID': 'ni:///sha-256;e65c3a997e77f34b58306da7a82ab0fc91c7820013287700f0b50345e5795b97?ver=CBV2.0',
+        'isA': 'TransformationEvent',
+        'eventTime': '2013-10-31T14:58:56.591Z',
+        'eventTimeZoneOffset': '+02:00',
+        'inputEPCList': ['urn:epc:id:sgtin:4012345.011122.25', 'urn:epc:id:sgtin:4000001.065432.99886655'],
+        'inputQuantityList': [{ 'epcClass': 'urn:epc:class:lgtin:4012345.011111.4444', 'quantity': 10, 'uom': 'KGM' },
+          { 'epcClass': 'urn:epc:class:lgtin:0614141.077777.987', 'quantity': 30 },
+          { 'epcClass': 'urn:epc:idpat:sgtin:4012345.066666.*', 'quantity': 220 },
+        ],
+        'outputEPCList': [
+          'urn:epc:id:sgtin:4012345.077889.25',
+          'urn:epc:id:sgtin:4012345.077889.26',
+          'urn:epc:id:sgtin:4012345.077889.27',
+          'urn:epc:id:sgtin:4012345.077889.28',
+        ],
+
+        'bizStep': 'urn:epcglobal:cbv:bizstep:commissioning',
+        'disposition': 'urn:epcglobal:cbv:disp:in_progress',
+        'readPoint': { 'id': 'urn:epc:id:sgln:4012345.00001.0' },
+        'ilmd': { 'example:bestBeforeDate': '2014-12-10', 'example:batch': 'XYZ' },
+        'example:myField': 'Example of a vendor/user extension',
+      },
+
+    ],
+  },
+
+};
+export const exampleAssociationEvent = {
+  '@context': ['https://gs1.github.io/EPCIS/epcis-context.jsonld', { 'example': 'http://ns.example.com/epcis/' }],
+
+  'id': '_:document1',
+  'isA': 'EPCISDocument',
+  'schemaVersion': '2.0',
+  'creationDate': '2019-11-06T14:00:00.000+01:00',
+  'epcisBody': {
+    'eventList': [
+      {
+        'eventID': 'ni:///sha-256;5f7c472bc4905de27a19b2efc8e4a9c6dc195139669b80b515f12218ff07cf65?ver=CBV2.0',
+        'isA': 'AssociationEvent',
+        'eventTime': '2019-11-06T14:00:00.000+01:00',
+        'recordTime': '2019-11-06T14:05:00.000+01:00',
+        'eventTimeZoneOffset': '+01:00',
+        'parentID': 'urn:epc:id:grai:4012345.55555.98765',
+        'childEPCs': ['urn:epc:id:giai:4000001.12345', 'urn:epc:id:giai:4000001.12346'],
+        'childQuantityList': [
+          { 'epcClass': 'urn:epc:class:lgtin:4023333.002000.998877', 'quantity': 4 },
+        ],
+        'action': 'ADD',
+        'bizStep': 'urn:epcglobal:cbv:bizstep:installing',
+        'disposition': 'urn:epcglobal:cbv:disp:in_progress',
+        'readPoint': { 'id': 'urn:epc:id:sgln:4012345.00001.0' },
+        'bizLocation': { 'id': 'urn:epc:id:sgln:4012345.00002.0' },
+        'bizTransactionList': [{
+          'type': 'urn:epcglobal:cbv:btt:inv',
+          'bizTransaction': 'urn:epcglobal:cbv:bt:4023333000000:54545',
+        }],
+        'sourceList': [
+          { 'type': 'urn:epcglobal:cbv:sdt:possessing_party', 'source': 'urn:epc:id:pgln:4000001.00012' },
+        ],
+        'destinationList': [
+          { 'type': 'urn:epcglobal:cbv:sdt:possessing_party', 'destination': 'urn:epc:id:pgln:4012345.00000' },
+        ],
+        'sensorElementList': [
+          {
+            'sensorMetadata': {
+              'startTime': '2019-11-06T13:55:00.000+01:00',
+              'endTime': '2019-11-06T13:57:00.000+01:00',
+            },
+            'sensorReport': [
+              { 'type': 'gs1:Humidity', 'minValue': 12.1, 'maxValue': 12.2, 'uom': 'A93' },
+            ],
+          },
+        ],
+      },
+
+    ],
+  },
+
 };
 
 export const exampleQuantityElement = {
@@ -266,39 +399,39 @@ export const exampleVocabulary = {
     'cbvmda:sst': 201,
   },
 
-  {
-    isA: 'VocabularyElement',
-    id: 'urn:epc:id:sgln:0037000.00729.8202',
-    'cbvmda:site': '0037000007296',
-    'cbvmda:sst': 202,
-  },
+    {
+      isA: 'VocabularyElement',
+      id: 'urn:epc:id:sgln:0037000.00729.8202',
+      'cbvmda:site': '0037000007296',
+      'cbvmda:sst': 202,
+    },
 
-  {
-    isA: 'urn:epcglobal:epcis:vtype:BusinessLocation',
-    id: 'urn:epc:id:sgln:0037000.00729.0',
-    attributes: [
-      { id: 'xmda:latitude', attribute: '+18.0000' },
-      { id: 'xmda:longitude', attribute: '-70.0000' },
-      {
-        id: 'xmda:address',
-        attribute: {
-          '@context': {
-            '@vocab': 'http://epcis.example.com/ns/',
+    {
+      isA: 'urn:epcglobal:epcis:vtype:BusinessLocation',
+      id: 'urn:epc:id:sgln:0037000.00729.0',
+      attributes: [
+        { id: 'xmda:latitude', attribute: '+18.0000' },
+        { id: 'xmda:longitude', attribute: '-70.0000' },
+        {
+          id: 'xmda:address',
+          attribute: {
+            '@context': {
+              '@vocab': 'http://epcis.example.com/ns/',
+            },
+            isA: 'Address',
+            street: '100 Nowhere Street',
+            city: 'Fancy',
+            state: 'DC',
+            zip: '99999',
           },
-          isA: 'Address',
-          street: '100 Nowhere Street',
-          city: 'Fancy',
-          state: 'DC',
-          zip: '99999',
         },
-      },
-    ],
-    children: [
-      'urn:epc:id:sgln:0037000.00729.8201',
-      'urn:epc:id:sgln:0037000.00729.8202',
-      'urn:epc:id:sgln:0037000.00729.8203',
-    ],
-  },
+      ],
+      children: [
+        'urn:epc:id:sgln:0037000.00729.8201',
+        'urn:epc:id:sgln:0037000.00729.8202',
+        'urn:epc:id:sgln:0037000.00729.8203',
+      ],
+    },
   ],
 };
 export const exampleVocabularyElements = exampleVocabulary.vocabularyElementList;
