c81e6c6f39e8f4cfad81ba1cd2b95036015ddae5
clementh59 <hecquet.c59@gmail.com>
2022-02-18 11:36:04 +0100

    The ILMD has now a type and format field

--- src/entity/model/Ilmd.js
+++ src/entity/model/Ilmd.js
@@ -8,4 +8,40 @@ import Entity from '../Entity';
 
 export default class Ilmd extends Entity {
 
+  /**
+   * Set the type property
+   * @param {string} type
+   * @return {Ilmd} - the ilmd instance
+   */
+  setType(type) {
+    this.type = type;
+    return this;
+  }
+
+  /**
+   * Getter for the type property
+   * @return {string} - the type
+   */
+  getType() {
+    return this.type;
+  }
+
+  /**
+   * Set the format property
+   * @param {string} format
+   * @return {Ilmd} - the ilmd instance
+   */
+  setFormat(format) {
+    this.format = format;
+    return this;
+  }
+
+  /**
+   * Getter for the format property
+   * @return {string} - the format
+   */
+  getFormat() {
+    return this.format;
+  }
+
 }
