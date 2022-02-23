a5a574d6d5285d21f505eb9ae07e18ae1ff75523
clementh59 <hecquet.c59@gmail.com>
2022-02-23 11:17:02 +0100

    Exported ExtendedEvent in index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export { default as EPCISDocument } from './entity/epcis/EPCISDocument';
 export { default as EPCISMasterData } from './entity/epcis/EPCISMasterData';
 export { default as EPCISHeader } from './entity/epcis/EPCISHeader';
 export { default as ObjectEvent } from './entity/events/ObjectEvent';
+export { default as ExtendedEvent } from './entity/events/ObjectEvent';
 export { default as TransformationEvent } from './entity/events/TransformationEvent';
 export { default as AggregationEvent } from './entity/events/AggregationEvent';
 export { default as AssociationEvent } from './entity/events/AssociationEvent';
