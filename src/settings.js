b027ad4b394dfbdb23115562562bf749476789ab
PercheEtienne <e.perche59@gmail.com>
2022-03-29 10:37:51 +0200

    add validationMode in default settings

--- src/settings.js
+++ src/settings.js
@@ -20,6 +20,9 @@
  * @property {string} EPCISDocumentSchemaVersion - The default value of the 'schemaVersion'
  * parameter of an EPCISDocument. Set it to undefined if you don't want the parameter in your
  * EPCISDocument.
+ * @property {string} validationMode The default value of 'validationMode' for the format 
+ * validation. Set it to "fast" mode if you want the following formats simplified: "date", 
+ * "time", "date-time", "iso-time", "iso-date-time", "uri", "uri-reference", "email".
  * */
 
 /**
@@ -37,6 +40,7 @@ export const defaultSettings = {
   EPCISDocumentContext: 'https://gs1.github.io/EPCIS/epcis-context.jsonld',
   EPCISDocumentSchemaVersion: '2.0',
   documentValidation: true,
+  validationMode: 'full',
 };
 
 // Initialize settings with defaults.
